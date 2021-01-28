49f78a6369abe467efeabfaed466f72d8a8786ca
Davaadorj Battulga <davaadorj.battulga@u-hopper.com>
2021-01-28 15:10:18 +0100

    added deploy & delete all script

---
+++ chirpstack-kubernetes/delete_all.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+kubectl delete -f ./mosquitto/
+kubectl delete -f ./influxdb/
+kubectl delete -f ./postgres/
+kubectl delete -k redis/.
+# kubectl delete -f ./chirpstack-gateway-bridge/
+kubectl delete -f ./chirpstack-network-server/
+kubectl delete -f ./chirpstack-application-server/
+kubectl delete -f ./monitoring/
+kubectl delete -f ./nodered/
+kubectl delete pvc mosquitto postgres-pv-claim postgresinit-pv-claim
+kubectl delete pv mosquitto-pv-volume postgres-pv-volume postgresinit-pv-volume
