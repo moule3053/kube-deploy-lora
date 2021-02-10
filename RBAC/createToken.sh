dec8edbd866b959885cf180738c82714afe02fbe
mozhdeh-farhadi <mojde.farhadi.ac@gmail.com>
2021-02-10 12:39:24 +0100

    create config for one cluster

--- RBAC/createToken.sh
+++ RBAC/createToken.sh
@@ -2,9 +2,8 @@ export varnamespace=team1-namespace
 export varsaname=team1-sa
 
 #get service account properties
-kubectl get serviceaccounts/$varsaname --namespace $varnamespace -o yaml
-
+name=$(kubectl get serviceaccounts/$varsaname --namespace $varnamespace -o jsonpath='{.secrets[0].name}')
 #get the token based on the output of the previous command
-#kubectl describe secrets/[] -n $varnamespace
+kubectl describe secrets/$name -n $varnamespace
 
 
